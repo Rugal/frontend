ec2a116386f1201f02c3601ee3d087895c3ac4fb
frances921 <taoa@uwindsor.ca>
2015-11-15 15:14:32 -0500

    commit
    meal type: breakfast, lunch, dinner; add tag for the meal, redesign the
    banner.gif; fetch data automaticlly; css style

--- js/app.js
+++ js/app.js
@@ -1,47 +1,16 @@
-var app = angular.module('myApp', []);
+    var foodApp = angular.module('foodApp', []);
 
-app.controller('restCtrl', function($scope, $http) {
 
-  $http.get("https://food-for-you.herokuapp.com/menu")
-   .success(function (response) {
-	  $scope.names = response.data;});
-  });
-  
-app.controller('button',['$scope','$http', function($scope, $http) {
-    $scope.triggerElement = true;
-    var myElement= document.getElementById('myElement');
-    angular.element(myElement).triggerHandler('click');
-
-    $scope.showbTriggerElement = function() {
-        $scope.triggerElement = false;
-		app.controller('restCtrl1', function($scope, $http) {
-
-			$http.get("https://food-for-you.herokuapp.com/menu?meal=breakfast")
-				.success(function (response) {
-					$scope.names = response.data;});
-		});
-	}
+    foodApp.controller('menuController',['$http', function($http){
 	
-	$scope.showlTriggerElement = function() {
-        $scope.triggerElement = false;
-		app.controller('restCtrl2', function($scope, $http) {
 
-			$http.get("https://food-for-you.herokuapp.com/menu?meal=lunch")
-				.success(function (response) {
-					$scope.names = response.data;});
-		});
-	}
-    $scope.showdTriggerElement = function() {
-        $scope.triggerElement = false;
-		app.controller('restCtrl3', function($scope, $http) {
-
-			$http.get("https://food-for-you.herokuapp.com/menu?meal=dinner")
-				.success(function (response) {
-					$scope.names = response.data;});
-		});
-	}
-	
-    $scope.hideTriggerElement = function() {
-        $scope.triggerElement = true;
-    }
-}]);
+        var rest=this;
+        rest.menu=[];
+					
+         this.send=function(mealtype){$http.get("https://food-for-you.herokuapp.com/menu?meal=" + mealtype).success(function(response){
+         //this.send=function(){$http.get('https://food-for-you.herokuapp.com/menu?meal=breakast').success(function(response){
+            
+			rest.menu=response.data;	});
+        		
+		};
+    }]);
