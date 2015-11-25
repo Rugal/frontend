0b1978182f7e9415295fc7c72a84875e7139c914
frances921 <taoa@uwindsor.ca>
2015-11-24 23:18:18 -0500

    commit
    final version, add google map, edit tag display, add map icon,

--- js/app.js
+++ js/app.js
@@ -3,14 +3,30 @@
 
     foodApp.controller('menuController',['$http', function($http){
 	
-
+		
         var rest=this;
         rest.menu=[];
 					
          this.send=function(mealtype){$http.get("https://food-for-you.herokuapp.com/menu?meal=" + mealtype).success(function(response){
-         //this.send=function(){$http.get('https://food-for-you.herokuapp.com/menu?meal=breakast').success(function(response){
             
-			rest.menu=response.data;	});
-        		
+			rest.menu=response.data;	
+        	var uri_encode= rest.menu.restaurant.map;
+			var uri = decodeURIComponent(uri_encode);
+			
+		
+			
+			document.getElementById("menu").innerHTML = "<iframe src="+"https://www.google.com/maps/embed?" +uri+", width=400px, height=300px></iframe>";
+			});
+
 		};
     }]);
+			
+	foodApp.controller('mapCtrl', function($scope){ 	
+		$scope.showmap = false;
+		
+			$scope.mapicon = function() {
+				$scope.showmap = !$scope.showmap;       
+			}
+
+	});
+	
